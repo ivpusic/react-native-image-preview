b868615750c0534bd88831431dccf6025ec2e8d9
Ivan Pusic <pusic007@gmail.com>
2016-06-04 20:58:34 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# react-native-image-preview
+Display zoomed image in transparent modal window
+
+## Demo
+![](http://i.imgur.com/lekjfbC.gif)
+
+## Installation
+
+```
+npm i react-native-image-preview
+```
+
+## Usage
+
+```Javascript
+import ImagePreview from 'react-native-image-preview';
+
+...
+
+<ImagePreview visible={visible} source={{uri: 'some-source'}} close={setVisibleToFalse} />
+
+```
+
+## License
+*MIT*
---
+++ index.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import {Modal, View, TouchableWithoutFeedback, Image, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  overlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.7)'
+  },
+  image: {
+    flex: 1,
+    marginLeft: 10,
+    marginRight: 10
+  }
+});
+
+export default class ImageZoom extends Component {
+  render() {
+    let {source, visible, close, imageStyle} = this.props;
+
+    return <Modal
+        animationType={'fade'}
+        transparent={true}
+        visible={visible}>
+      <View style={styles.overlay}>
+        <TouchableWithoutFeedback onPress={close}>
+          <Image resizeMode={'contain'} source={source} style={[styles.image, imageStyle]} />
+        </TouchableWithoutFeedback>
+      </View>
+    </Modal>;
+  }
+}
+
+ImageZoom.propTypes = {
+  visible: React.PropTypes.bool,
+  close: React.PropTypes.func,
+  source: React.PropTypes.object,
+  imageStyle: React.PropTypes.oneOfType([
+    React.PropTypes.number,
+    React.PropTypes.object
+  ]),
+  children: React.PropTypes.oneOfType([
+    React.PropTypes.object,
+    React.PropTypes.array
+  ])
+};
+
